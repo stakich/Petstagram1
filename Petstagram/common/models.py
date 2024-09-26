5c39c0a8edcbf0a636b9c3a76cafea801426efc7
stakich <stanislav030507e@gmail.com>
2024-09-26 20:15:41 +0300

    Implemented Like and Comment model

--- Petstagram/common/models.py
+++ Petstagram/common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Petstagram.photos.models import Photo
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=300)
+    date_time_of_publication = models.DateTimeField(auto_now_add=True)
+    to_photo = models.ForeignKey(to=Photo, on_delete=models.CASCADE, related_name='comments')
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(to=Photo, on_delete=models.CASCADE)
--- Petstagram/photos/admin.py
+++ Petstagram/photos/admin.py
@@ -5,4 +5,8 @@ from Petstagram.photos.models import Photo
 # Register your models here.
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['location', 'description']
+    list_display = ['id', 'date_of_publication', 'description', 'get_tagged_pets']
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join([pet.name for pet in obj.tagged_pets.all()])
