d096deaf02b3097b590b23e0c168431756545dd5
stakich <stanislav030507e@gmail.com>
2024-10-08 18:54:30 +0300

    Optimized HTML and added Like functionality

--- Petstagram/common/urls.py
+++ Petstagram/common/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from Petstagram.common import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like')
 ]
--- Petstagram/common/views.py
+++ Petstagram/common/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Petstagram.photos.models import Photo
+from Petstagram.common.models import Like
 
 
 # Create your views here.
+
 def home_page(request):
-    return render(request, 'common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {
+        'all_photos': all_photos
+    }
+
+    return render(request, 'common/home-page.html', context)
+
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(pk=photo_id)
+
+    liked_object = Like.objects.filter(to_photo_id=photo_id).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo=photo)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
